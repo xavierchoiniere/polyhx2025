f4f139daa874ac1a2679bd059d476a4efbf3f7ff
xavierchoiniere <xavier.choiniere@gmail.com>
2025-02-08 18:51:16 -0500

    feed completed

--- fishstagram/src/app/pages/feed-page/feed-page.component.ts
+++ fishstagram/src/app/pages/feed-page/feed-page.component.ts
@@ -4,16 +4,20 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { Publication } from '@common/publication'
+import { Fish } from '@common/fish'
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-feed-page',
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule, DatePipe],
   templateUrl: './feed-page.component.html',
   styleUrl: './feed-page.component.css'
 })
 export class FeedPageComponent {
   constructor(private router: Router) {}
 
+  publications: Publication[] = []; //get list from DB
   openPost(){
     this.router.navigate(['/post']);
   }
