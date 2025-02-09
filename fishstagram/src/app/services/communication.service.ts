bd43bdf6126db06e25ceca92c556fb93efff54f8
xavierchoiniere <xavier.choiniere@gmail.com>
2025-02-08 20:11:23 -0500

    user fonctions communication service

--- fishstagram/src/app/services/communication.service.ts
+++ fishstagram/src/app/services/communication.service.ts
@@ -1,7 +1,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError  } from 'rxjs';
 import { User } from '@common/user'
 
 @Injectable({
@@ -14,6 +14,14 @@ export class CommunicationService {
 
   signup(user: User): Observable<User> {
     return this.http.post<User>('${this.apiUrl}/signup', user);
+  }
+  
+  login(email: string, password: string): Observable<User> {
+    return this.http.post<User>('${this.apiUrl}/login', { email, password });
+  }
+
+  getUser(username: string): Observable<User> {
+    return this.http.get<User>('${this.baseUrl}/account/${username}');
   }
 
 }
