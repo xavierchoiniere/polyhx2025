d58da6a8bfec80bfa51b1dffeae00d7d2f32efb4
xavierchoiniere <xavier.choiniere@gmail.com>
2025-02-09 00:09:40 -0500

    get pubs by user working

--- fishstagram/src/app/services/communication.service.ts
+++ fishstagram/src/app/services/communication.service.ts
@@ -27,7 +27,7 @@ export class CommunicationService {
     return this.http.get<Publication[]>(`${this.apiUrl}/publications`);
   }
   getPublicationsByUser(username: string): Observable<Publication[]> {
-    return this.http.get<Publication[]>(`${this.apiUrl}/publications/${username}`);
+    return this.http.get<Publication[]>(`${this.apiUrl}/publications/user/${username}`);
   }
 
   getUser(username: string): Observable<User> {
