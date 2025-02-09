99f344755ee86d37bcbf09513a32b762ed242b7e
Jonathan Boyle <jboyle9823@gmail.com>
2025-02-09 00:01:57 -0500

    small commit

--- fishstagram/src/app/pages/account-page/account-page.component.ts
+++ fishstagram/src/app/pages/account-page/account-page.component.ts
@@ -4,6 +4,7 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { DatePipe } from '@angular/common';
 import { Publication } from '@common/publication'
+import { CommunicationService } from '../../services/communication.service';
 
 @Component({
   selector: 'app-account-page',
@@ -13,10 +14,18 @@ import { Publication } from '@common/publication'
 })
 
 export class AccountPageComponent {
+  constructor(private communicationService: CommunicationService) {}
   currentScreen: string = 'Publications'; 
   isScientist: boolean = true; 
 
-  
+  publications: Publication[] = [];
+
+  ngOnInit() {
+    this.communicationService.getPublicationsByUser(sessionStorage.getItem('username') as string).subscribe({
+      next: (response) => {
+        this.publications = response;
+      }});
+  }
 
   onToggleChange(event: any) {
     this.currentScreen = event.value;
