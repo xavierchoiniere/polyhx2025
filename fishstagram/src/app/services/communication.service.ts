10fdaa5f0a64e9b7a07d19a7aeb003b5f1310bfb
xavierchoiniere <xavier.choiniere@gmail.com>
2025-02-08 21:55:52 -0500

    anything

--- fishstagram/src/app/services/communication.service.ts
+++ fishstagram/src/app/services/communication.service.ts
@@ -13,15 +13,15 @@ export class CommunicationService {
   constructor(private http: HttpClient) {}
 
   signup(user: User): Observable<User> {
-    return this.http.post<User>('${this.apiUrl}/signup', user);
+    return this.http.post<User>(`${this.apiUrl}/signup`, user);
   }
   
-  login(email: string, password: string): Observable<User> {
-    return this.http.post<User>('${this.apiUrl}/login', { email, password });
+  login(username: string, password: string): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/users/login`, { username, password });
   }
 
   getUser(username: string): Observable<User> {
-    return this.http.get<User>('${this.baseUrl}/account/${username}');
+    return this.http.get<User>(`${this.apiUrl}/account/${username}`);
   }
 
 }
--- server/app/index.ts
+++ server/app/index.ts
@@ -1,7 +1,6 @@
 import { AppModule } from '@app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { PopulateService } from '@app/services/populate.service';
 
 const bootstrap = async () => {
     const app = await NestFactory.create(AppModule);
@@ -9,9 +8,7 @@ const bootstrap = async () => {
     app.useGlobalPipes(new ValidationPipe());
     app.enableCors();
 
-    const populateService = app.get(PopulateService);
-
-    await app.listen(process.env.PORT);
+    await app.listen(process.env.PORT || 3000);
     
 };
 
