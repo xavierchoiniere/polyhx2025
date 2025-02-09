b6665926ee7a69bbd33a73d020dd4516bfc8f63e
aziz-mrf <azizmaaref@gmail,com>
2025-02-08 22:06:01 -0500

    fix login

--- fishstagram/src/app/pages/login-page/login-page.component.ts
+++ fishstagram/src/app/pages/login-page/login-page.component.ts
@@ -3,31 +3,47 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CommunicationService } from '../../services/communication.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-login-page',
   imports: [FormsModule],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
   user = '';
   password = '';
-  constructor(private router: Router, private communicationService: CommunicationService, private snackBar: MatSnackBar) {}
-  connect(){
-    this.communicationService.login(this.user, this.password).subscribe(
-      {next: (response)=> {if (response){
-        this.router.navigate(['/feed']);}
-        else this.snackBar.open('Wrong password.', 'Close', {duration:3000});
-      }})
+  constructor(
+    private router: Router,
+    private communicationService: CommunicationService,
+    private snackBar: MatSnackBar
+  ) {}
 
+  connect() {
+    this.communicationService.login(this.user, this.password).subscribe({
+      next: (response) => {
+        if (response) {
+          this.router.navigate(['/feed']);
+        } else {
+          this.snackBar.open('Wrong password.', 'Close', { duration: 3000 });
+        }
+      },
+      error: (error) => {
+        this.snackBar.open('Login failed.', 'Close', { duration: 3000 });
+      },
+    });
   }
-  signUp(){
+
+  signUp() {
     this.router.navigate(['/signup']);
   }
+
   focusPassword() {
-    const passwordInput = document.querySelector('input[type="password"]') as HTMLInputElement;
+    const passwordInput = document.querySelector(
+      'input[type="password"]'
+    ) as HTMLInputElement;
     if (passwordInput) {
-        passwordInput.focus();
+      passwordInput.focus();
     }
   }
 }
--- fishstagram/src/app/services/communication.service.ts
+++ fishstagram/src/app/services/communication.service.ts
@@ -1,27 +1,28 @@
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError  } from 'rxjs';
-import { User } from '@common/user'
+import { Observable } from 'rxjs';
+import { User } from '@common/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CommunicationService {
-  private readonly apiUrl: string = 'http://localhost:4200/api';
+  private readonly apiUrl: string = 'http://localhost:3000/api';
 
   constructor(private http: HttpClient) {}
 
   signup(user: User): Observable<User> {
-    return this.http.post<User>(`${this.apiUrl}/signup`, user);
+    return this.http.post<User>(`${this.apiUrl}/users/signup`, user);
   }
-  
+
   login(username: string, password: string): Observable<User> {
-    return this.http.post<User>(`${this.apiUrl}/users/login`, { username, password });
+    return this.http.post<User>(`${this.apiUrl}/users/login`, {
+      username,
+      password,
+    });
   }
 
   getUser(username: string): Observable<User> {
-    return this.http.get<User>(`${this.apiUrl}/account/${username}`);
+    return this.http.get<User>(`${this.apiUrl}/users/${username}`);
   }
-
 }
