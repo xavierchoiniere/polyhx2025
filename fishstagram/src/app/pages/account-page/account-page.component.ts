ed80a2bf57ca2fe9abc619ef12898e471b5eaf41
Jonathan Boyle <jboyle9823@gmail.com>
2025-02-08 21:15:06 -0500

    added post functionality

--- fishstagram/src/app/pages/account-page/account-page.component.ts
+++ fishstagram/src/app/pages/account-page/account-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { Publication } from '@common/publication'
+import { Fish } from '@common/fish'
 
 @Component({
   selector: 'app-account-page',
@@ -8,9 +10,12 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
   templateUrl: './account-page.component.html',
   styleUrls: ['./account-page.component.css'],
 })
+
 export class AccountPageComponent {
   currentScreen: string = 'posts'; 
   isScientist: boolean = false; 
+
+  publications: Publication[] = [];
 
   onToggleChange(event: any) {
     this.currentScreen = event.value;
