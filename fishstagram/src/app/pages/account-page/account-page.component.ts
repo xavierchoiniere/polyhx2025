af39ee6e35c57e657a16af929b911effb9859278
Jonathan Boyle <jboyle9823@gmail.com>
2025-02-09 01:17:08 -0500

    Added basic leaderboards

--- fishstagram/src/app/pages/account-page/account-page.component.ts
+++ fishstagram/src/app/pages/account-page/account-page.component.ts
@@ -5,10 +5,11 @@ import { MatCardModule } from '@angular/material/card';
 import { DatePipe } from '@angular/common';
 import { Publication } from '@common/publication'
 import { CommunicationService } from '../../services/communication.service';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-account-page',
-  imports: [MatIconModule, MatButtonToggleModule, MatCardModule, DatePipe],
+  imports: [MatIconModule, MatButtonToggleModule, MatCardModule, DatePipe, MatListModule],
   templateUrl: './account-page.component.html',
   styleUrls: ['./account-page.component.css'],
 })
@@ -19,6 +20,11 @@ export class AccountPageComponent {
   isScientist: boolean = true; 
 
   publications: Publication[] = [];
+  leaderboard = [
+    { id: 1, icon: 'https://example.com/icon1.png', name: 'User One', level: 10 },
+    { id: 2, icon: 'https://example.com/icon2.png', name: 'User Two', level: 8 },
+    { id: 3, icon: 'https://example.com/icon3.png', name: 'User Three', level: 7 },
+  ];
 
   ngOnInit() {
     this.communicationService.getPublicationsByUser(sessionStorage.getItem('username') as string).subscribe({
